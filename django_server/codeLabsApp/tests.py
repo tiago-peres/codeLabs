635a6bb362b2f8509ebd260809e37435cd300d78
Tiago Peres <tiagomartinsperes@gmail.com>
2020-06-03 11:31:50 +0100

    one test is still failing

--- django_server/codeLabsApp/tests.py
+++ django_server/codeLabsApp/tests.py
@@ -5,10 +5,6 @@ from codeLabsApp.models import MyUser
 from rest_framework import status
 # Create your tests here.
 
-'''
-FROM: USER APP
-'''
-# Yet to consider possible user registration errors
 class MyUserTest(APITestCase):
     def setUp(self):
         # Create a user
@@ -18,9 +14,9 @@ class MyUserTest(APITestCase):
         self.create_url = reverse('user-create')
 
     def test_create_user(self):
-        """
-        Ensure we can create a new user and a valid token is created with it.
-        """
+        '''
+        Ensure we can create a new user.
+        '''
         data = {
             'username': 'tiagoperes',
             'password': 'test'
@@ -36,5 +32,82 @@ class MyUserTest(APITestCase):
         self.assertEqual(response.data['username'], data['username'])
         self.assertFalse('password' in response.data)
 
-    #TODO test creating user with too long username, with preexisting username, with older / earlier date than today...
+    def test_create_user_with_short_password(self):
+        '''
+        Ensure we can't create a user with short password.
+        '''
+        data = {
+            'username': 'tiagoperes',
+            'password': 'eii'
+        }
+
+        response = self.client.post(self.create_url , data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(MyUser.objects.count(), 1)
+        self.assertEqual(len(response.data['password']), 1)
+
+
+    def test_create_user_with_no_password(self):
+        '''
+        Ensure we can't create a user with no password.
+        '''
+        data = {
+            'username': 'tiagoperes',
+            'password': ''
+        }
+
+        response = self.client.post(self.create_url , data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(MyUser.objects.count(), 1)
+        self.assertEqual(len(response.data['password']), 1)
+
+
+    def test_create_user_with_no_username(self):
+        '''
+        Ensure we can't create a user with no username.
+        '''
+        data = {
+            'username': '',
+            'password': 'test'
+        }
+
+        response = self.client.post(self.create_url , data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(MyUser.objects.count(), 1)
+        self.assertEqual(len(response.data['username']), 1)
+
+    
+    def test_create_user_with_too_long_username(self):
+        '''
+        Ensure we can't create a user with too long username.
+        '''
+        data = {
+            'username': 'tiagoperes'*50,
+            'password': 'test'
+        }
+
+        response = self.client.post(self.create_url , data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(MyUser.objects.count(), 1)
+        self.assertEqual(len(response.data['username']), 1)
+
+
+    def test_create_user_with_preexisting_username(self):
+        '''
+        Ensure we can't create a user with preexisting username.
+        '''
+        data = {
+            'username': 'testing',
+            'password': 'test'
+        }
+
+        response = self.client.post(self.create_url , data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(MyUser.objects.count(), 1)
+        self.assertEqual(len(response.data['username']), 1)
 
